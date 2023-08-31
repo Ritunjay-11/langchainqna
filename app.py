9d23e142525ce97fed4cab2fc8a5b9071545bcd7
Ritunjay-11 <66157748+Ritunjay-11@users.noreply.github.com>
2023-08-31 09:11:47 +0530

    final app.py

--- app.py
+++ app.py
@@ -7,7 +7,20 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
+from langchain.llms import OpenAIChat
+from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
+from prompt import get_completion
 
+
+def contradict(answer):
+    #user_q = st.text_input(label = "", key="new_input", placeholder="Ask a question about your PDF:")
+    
+
+    prompt = answer + "\n" + "Given above are a list of requirements from a client for a company that builds EV charging software. Point out which of these statements might be contradictory to each other, along with scenarios of failure"
+    final_ans = get_completion(prompt)
+    res = final_ans
+    return res
 def main():
     load_dotenv()
     st.set_page_config(page_title="Ask your PDF")
@@ -27,16 +40,34 @@ def main():
         documents = FAISS.from_texts(chunks, embeddings)
 
         user_question = st.text_input("Ask a question about your PDF:")
+        ans = ""
         if user_question:
             # Perform similarity search to get relevant documents for the user's question
             docs = documents.similarity_search(user_question)
 
+            
+
+            
             llm = OpenAI()
-            chain = load_qa_chain(llm, chain_type="stuff")
+            #chain = load_qa_chain(llm, chain_type="stuff")
+            qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=documents.as_retriever(), return_source_documents=True)
+            result = qa({"query":user_question})
+            ans+=result["result"]
 
-            # Perform question answering using the defined chain
-            response = chain.run(input_documents=docs, question=user_question)
-            st.write(response)
+            st.write(result["result"])
+           
+    
 
+    resp = st.button("Generate Contradictions")
+
+    if resp:
+        
+        if ans == "":
+            st.write("No context provided. Please ask an appropriate question")
+        else:
+            resp = contradict(ans)
+            st.write(resp)
+            
+            
 if __name__ == "__main__":
     main()
